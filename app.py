3a716c5fc4455a9594c293970d809c7404f43352
Kim-songhui <156580769+Kim-songhui@users.noreply.github.com>
2025-06-01 19:08:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 app.register_blueprint(recommend_bp, url_prefix="/")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5050)
+    app.run(host='0.0.0.0', port=5000)
