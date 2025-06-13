6108af9efa2517c8a4cb1f0f95352ea0145cf68e
Kim-songhui <rlathdgmlkk@naver.com>
2025-06-13 20:43:28 +0900

    fix:403오류 수정

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,13 @@ from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)
+    CORS(app, resources={
+        r"/*": {
+            "origins": ["*"],  # 모든 도메인에서의 접근 허용
+            "methods": ["GET", "POST", "OPTIONS"],  # 허용할 HTTP 메서드
+            "allow_headers": ["Content-Type", "Authorization"]  # 허용할 헤더
+        }
+    })
 
     from app.routes.recommend import recommend_bp
     from app.routes.audio_to_text import audio_bp
